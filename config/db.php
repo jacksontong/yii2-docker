5a2882985db333d55b5d91478b35c3a8265c5e99
Jackson Tong <tongtoan2704@gmail.com>
2017-03-19 16:03:04 +0700

    Mysql intergration

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysql;dbname=' . getenv('MYSQL_DATABASE'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
 ];
